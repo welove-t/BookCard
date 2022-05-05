3fb5b9c0f78ee8ce3adfed7fbce47b51d92cc132
welove-t <tlove.tky@gmail.com>
2022-05-05 12:19:09 +0900

    ✨ フッター作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-green-200">
-      <header className="container flex h-16 items-center">
-        <span className=" text-lg text-blue-500">ブックカード</span>
-      </header>
+    <div>
+      <div className="bg-green-200">
+        <header className="container flex h-16 items-center">
+          <span className=" text-lg text-blue-500">ブックカード</span>
+        </header>
+      </div>
+      <div className="bg-gray-100 pt-10 pb-24">
+        <div className="container">
+          <div className="flex justify-center space-x-4">
+            <Link href="/">
+              <a className="text-gray-600">利用規約</a>
+            </Link>
+            <Link href="/">
+              <a className="text-gray-600">プライバシーポリシー</a>
+            </Link>
+          </div>
+          <div className="pt-4 text-center">
+            <Link href="/">
+              <a className="text-gray-600">当サービスの開発者</a>
+            </Link>
+            <p className="pt-4">&copy; BookCard</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
