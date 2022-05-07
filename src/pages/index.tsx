ffb710a93f32ce4694537761f84f7e137259bfd6
welove-t <tlove.tky@gmail.com>
2022-05-07 11:00:14 +0900

    ✨ （とりあえずの）ファーストビュー作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,17 @@ const Home: NextPage = () => {
           <span className=" text-lg text-blue-500">ブックカード</span>
         </header>
       </div>
+      <div className="container text-center">
+        <span>メインコンポーネント</span>
+        <br />
+        <span>オリジナルのブックカードを作ろう!</span>
+        <br />
+        <span>（ブックカードのイメージをカルーセルで表示させる）</span>
+        <br />
+        <button className="rounded-full border bg-green-500 px-4 py-2">
+          さっそく始める
+        </button>
+      </div>
       <div className="bg-gray-100 pt-10 pb-24">
         <div className="container">
           <div className="flex justify-center space-x-4">
